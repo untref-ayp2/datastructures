f4725c2bf5b3148f76768cebb17fd00b4f2f75bd
lagofa <falago@gmail.com>
2024-04-08 16:15:10 -0300

    quito new de la interfaz

--- set/set.go
+++ set/set.go
@@ -3,7 +3,6 @@ package set
 import "github.com/untref-ayp2/data-structures/types"
 
 type Set[T types.Ordered] interface {
-	NewSet(elements ...T) *Set[T]
 	Contains(element T) bool
 	Add(element T)
 	Remove(element T)
