76ae3c26abcd4ddcb5832f670fdae4657a6ed0d2
lagofa <falago@gmail.com>
2024-04-10 11:21:43 -0300

    Quito i al obtener values

--- set/set_list.go
+++ set/set_list.go
@@ -33,10 +33,8 @@ func (s *SetList[T]) Size() int {
 
 func (s *SetList[T]) Values() []T {
 	var values []T
-	i := 0
 	for node := s.elements.Head(); node != nil; node = node.Next() {
 		values = append(values, node.Data())
-		i++
 	}
 	return values
 }
