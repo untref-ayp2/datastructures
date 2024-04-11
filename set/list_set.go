5776f1abac91aef349c7e05d29e9fef3725b130e
Lucas Videla <videla.lucas@gmail.com>
2024-04-11 14:44:48 -0300

    Fixed SetList -> ListSet, based on name convention

--- set/list_set.go
+++ set/list_set.go
@@ -7,22 +7,22 @@ import (
 	"github.com/untref-ayp2/data-structures/types"
 )
 
-// SetList implementa un conjunto sobre una lista enlazada simple.
-type SetList[T types.Ordered] struct {
+// ListSet implementa un conjunto sobre una lista enlazada simple.
+type ListSet[T types.Ordered] struct {
 	elements *list.LinkedList[T]
 }
 
-func NewSetList[T types.Ordered](elements ...T) *SetList[T] {
-	set := &SetList[T]{list.NewLinkedList[T]()}
+func NewListSet[T types.Ordered](elements ...T) *ListSet[T] {
+	set := &ListSet[T]{list.NewLinkedList[T]()}
 	set.Add(elements...)
 	return set
 }
 
-func (s *SetList[T]) Contains(element T) bool {
+func (s *ListSet[T]) Contains(element T) bool {
 	return s.elements.Find(element) != nil
 }
 
-func (s *SetList[T]) Add(elements ...T) {
+func (s *ListSet[T]) Add(elements ...T) {
 	for _, element := range elements {
 		if !s.Contains(element) {
 			s.elements.Append(element)
@@ -30,15 +30,15 @@ func (s *SetList[T]) Add(elements ...T) {
 	}
 }
 
-func (s *SetList[T]) Remove(element T) {
+func (s *ListSet[T]) Remove(element T) {
 	s.elements.Remove(element)
 }
 
-func (s *SetList[T]) Size() int {
+func (s *ListSet[T]) Size() int {
 	return s.elements.Size()
 }
 
-func (s *SetList[T]) Values() []T {
+func (s *ListSet[T]) Values() []T {
 	var values []T
 	for node := s.elements.Head(); node != nil; node = node.Next() {
 		values = append(values, node.Data())
@@ -46,7 +46,7 @@ func (s *SetList[T]) Values() []T {
 	return values
 }
 
-func (s *SetList[T]) String() string {
+func (s *ListSet[T]) String() string {
 	result := "Conjunto: {"
 	for _, v := range s.Values() {
 		result += " " + fmt.Sprintf("%v", v)
