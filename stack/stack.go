466e14683dce9fb0ff9798d46f99591e68e06ca3
Lucas Videla <videla.lucas@gmail.com>
2024-04-16 09:08:58 -0300

    Enhanced docs and tests

--- stack/stack.go
+++ stack/stack.go
@@ -1,23 +1,43 @@
+// Package stack proporciona una implementación de una pila genérica sobre un arreglo dinámico.
+// Expone la estructura y funciones básicas para manipular una pila.
 package stack
 
 import "errors"
 
-// Stack implementa una pila genérica sobre un arreglo dinámico.
+// Proporciona una pila cuyos elementos son de un tipo genérico.
+// La implementación se basa en un arreglo dinámico.
 type Stack[T any] struct {
 	data []T
 }
 
-// New crea una nueva pila vacía.
+// Crea una nueva pila vacía.
+//
+// Uso:
+//
+//	s := stack.New[int]() // Crea una pila de enteros.
 func New[T any]() *Stack[T] {
 	return &Stack[T]{}
 }
 
-// Push agrega un elemento a la pila.
+// Agrega un elemento a la pila.
+//
+// Uso:
+//
+//	s.Push(10) // Agrega el entero 10 a la pila.
 func (s *Stack[T]) Push(x T) {
 	s.data = append(s.data, x)
 }
 
-// Pop remueve y retorna el elemento en el tope de la pila.
+// Remueve y retorna el elemento en el tope de la pila.
+// Si la pila está vacía, retorna un error.
+//
+// Uso:
+//
+//	if x, err := s.Pop(); err != nil {
+//		fmt.Println(err)
+//	} else {
+//		fmt.Println(x)
+//	}
 func (s *Stack[T]) Pop() (T, error) {
 	var x T
 	if s.IsEmpty() {
@@ -28,7 +48,17 @@ func (s *Stack[T]) Pop() (T, error) {
 	return x, nil
 }
 
-// Top retorna el elemento en el tope de la pila.
+// Retorna el elemento en el tope de la pila.
+// Si la pila está vacía, retorna un error.
+//
+// Uso:
+//
+//	if x, err := s.Top(); err != nil {
+//		fmt.Println(err)
+//	} else {
+//		fmt.Println(x)
+//	}
+
 func (s *Stack[T]) Top() (T, error) {
 	var x T
 	if s.IsEmpty() {
@@ -38,7 +68,14 @@ func (s *Stack[T]) Top() (T, error) {
 	return x, nil
 }
 
-// IsEmpty retorna true si la pila está vacía.
+// Retorna true si la pila está vacía.
+//
+// Uso:
+//
+//	if s.IsEmpty() {
+//		fmt.Println("La pila está vacía")
+//	}
+
 func (s *Stack[T]) IsEmpty() bool {
 	return len(s.data) == 0
 }
