5bfe299f8d7b5e1284689fd7556ae3e2459002b9
Lucas Videla <videla.lucas@gmail.com>
2024-04-23 09:06:55 -0300

    Enhanced bitmap tests and godocs

--- bitmap/bitmap_test.go
+++ bitmap/bitmap_test.go
@@ -6,7 +6,39 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestEncenderTodosDeAUno(t *testing.T) {
+func TestNuevoBitMapTodoEnCero(t *testing.T) {
+	m := NewBitMap()
+	assert.Equal(t, uint32(0), m.GetMap())
+}
+
+func TestEncenderUnBit(t *testing.T) {
+	m := NewBitMap()
+	m.On(1)
+	isOn, _ := m.IsOn(1)
+	assert.True(t, isOn)
+}
+
+func TestEncenderUnBitEnPosicionNoValida(t *testing.T) {
+	m := NewBitMap()
+	err := m.On(32)
+	assert.EqualError(t, err, "posición no válida")
+}
+
+func TestApagarUnBit(t *testing.T) {
+	m := NewBitMap()
+	m.On(1)
+	m.Off(1)
+	isOn, _ := m.IsOn(1)
+	assert.False(t, isOn)
+}
+
+func TestApagarUnBitEnPosicionNoValida(t *testing.T) {
+	m := NewBitMap()
+	err := m.Off(32)
+	assert.EqualError(t, err, "posición no válida")
+}
+
+func TestEncenderTodosLosBits(t *testing.T) {
 	m := NewBitMap()
 	for i := uint8(0); i < 32; i++ {
 		m.On(i)
@@ -16,16 +48,40 @@ func TestEncenderTodosDeAUno(t *testing.T) {
 
 }
 
-func TestQueSigaEncendido(t *testing.T) {
+func TestApagarTodosLosBits(t *testing.T) {
 	m := NewBitMap()
-	m.On(1)
-	m.On(1)
-	is1On, _ := m.IsOn(1)
-	assert.True(t, is1On)
+	for i := uint8(0); i < 32; i++ {
+		m.On(i)
+	}
+	for i := uint8(0); i < 32; i++ {
+		m.Off(i)
+	}
+	assert.Equal(t, uint32(0), m.GetMap())
 }
 
-func TestPosicionExcedida(t *testing.T) {
+func TestEstadoDeUnBit(t *testing.T) {
 	m := NewBitMap()
-	err := m.On(32)
-	assert.EqualError(t, err, "posición inválida")
+	m.On(1)
+	isOn, _ := m.IsOn(1)
+	assert.True(t, isOn)
+}
+
+func TestEstadoDeUnBitEnPosicionNoValida(t *testing.T) {
+	m := NewBitMap()
+	_, err := m.IsOn(32)
+	assert.EqualError(t, err, "posición no válida")
+}
+
+func TestEncenderVariasVecesUnMismoBitNoLoApaga(t *testing.T) {
+	m := NewBitMap()
+	m.On(1)
+	m.On(1)
+	isOn, _ := m.IsOn(1)
+	assert.True(t, isOn)
+}
+
+func TestGetMap(t *testing.T) {
+	m := NewBitMap()
+	m.On(1)
+	assert.Equal(t, uint32(2), m.GetMap())
 }
