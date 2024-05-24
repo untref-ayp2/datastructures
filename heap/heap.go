8397dea63efb70dc3f519a4c01444a4a605191ce
Martin Albarracin <mafranzone@untref.edu.ar>
2024-05-24 10:40:42 -0300

    Heap Generico

--- heap/heap.go
+++ heap/heap.go
@@ -1,22 +1,34 @@
 package heap
 
 import (
+	"cmp"
 	"errors"
 
 	"github.com/untref-ayp2/data-structures/types"
 )
 
-type Heap[T types.Ordered] struct {
+type Heap[T any] struct {
+	// contenedor de datos
 	elements  []T
-	isMinHeap bool
+	//Función de comparación. Para un heap de mínimo,
+	// devuelve 1 si a < b, 0 si a == b, -1 si a > b
+	// Para un heap de máximo, devuelve 1 si a > b, 0 si a == b, -1 si a < b
+	comp func(a T, b T) int  
 }
 
 func NewMinHeap[T types.Ordered]() *Heap[T] {
-	return &Heap[T]{isMinHeap: true}
+	return &Heap[T]{comp: cmp.Compare[T], elements: make([]T, 0)}
 }
 
 func NewMaxHeap[T types.Ordered]() *Heap[T] {
-	return &Heap[T]{isMinHeap: false}
+	comp:= func(a T, b T) int {
+		return cmp.Compare[T](b, a)
+	}
+	return &Heap[T]{comp: comp, elements: make([]T, 0)}
+}
+
+func NewGenericHeap[T any](comp func(a T, b T) int) *Heap[T] {
+	return &Heap[T]{comp: comp, elements: make([]T, 0)}
 }
 
 func (m *Heap[T]) Size() int {
@@ -31,7 +43,7 @@ func (m *Heap[T]) Insert(element T) {
 func (m *Heap[T]) upHeap(i int) {
 	for i > 0 {
 		parent := (i - 1) / 2
-		if !m.compare(m.elements[i], m.elements[parent]) {
+		if m.comp(m.elements[i], m.elements[parent]) == 1 {
 			break
 		}
 		m.elements[i], m.elements[parent] = m.elements[parent], m.elements[i]
@@ -56,33 +68,21 @@ func (m *Heap[T]) downHeap(i int) {
 	for {
 		left := 2*i + 1
 		right := 2*i + 2
-		if left >= m.Size() {
+		smallest := i
+
+		if left < m.Size() && m.comp(m.elements[left], m.elements[smallest]) == -1 {
+			smallest = left
+		}
+
+		if right < m.Size() && m.comp(m.elements[right], m.elements[smallest]) == -1 {
+			smallest = right
+		}
+
+		if smallest == i {
 			break
 		}
-		aux := left
-		if right < m.Size() && m.compare(m.elements[right], m.elements[left]) {
-			aux = right
-		}
-		if m.compareOrEqual(m.elements[i], m.elements[aux]) {
-			break
-		}
-		m.elements[i], m.elements[aux] = m.elements[aux], m.elements[i]
-		i = aux
+
+		m.elements[i], m.elements[smallest] = m.elements[smallest], m.elements[i]
+		i = smallest
 	}
-}
-
-func (m *Heap[T]) compareOrEqual(a T, b T) bool {
-	if m.isMinHeap {
-		return a <= b
-	}
-
-	return a >= b
-}
-
-func (m *Heap[T]) compare(a T, b T) bool {
-	if m.isMinHeap {
-		return a < b
-	}
-
-	return a > b
 }
